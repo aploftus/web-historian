2600c2187b982d58c81d38b4bceed839f07ba329
Hack Reactor Student <student@hackreactor.com>
2017-11-01 19:25:38 -0700

    Return content of archived websites

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,36 +1,26 @@
 var fs = require('fs');
-var headers = require('./http-helpers');
+var {headers} = require('./http-helpers');
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var url = require('url'); // native url module
 
 exports.handleRequest = function (req, res) {
   var statusCode = 200;
-  // console.log(req.url);
-  var urlPath = url.parse(req.url).pathname;
-  var indexPath = archive.paths.siteAssets + '/index.html';
-  // url.parse parses the request url and grab pathname property off parsed url obj
+  console.log('URL: ', req.url);
   
-  if (urlPath === '/') {  
-    // var index = fs.open(archive.paths.siteAssets + '/index.html', 'r', (err, fd) => {
-      // console.log('ERROR : ', err, fd);
-      // return fd;
-      // file description: position in memory
-    // });
-    // console.log('------ ', index.read());
-    
-    // fs.open(indexPath, 'r', (err, fd) => {
-    //   res.end(JSON.stringify(fs.read(fd)));
-    // });
-    
-    fs.readFile(indexPath, (err, data) => {
+  if (req.url === '/') {
+    var sitePath = archive.paths.siteAssets + '/index.html';
+  } else {
+    var sitePath = archive.paths.archivedSites + req.url;
+  }
+  
+  if (sitePath) {
+    fs.readFile(sitePath, (err, data) => {
       if (!err) {
-        console.log('SUCCESS : ', data);
-        res.writeHead(statusCode, headers.headers);
+        res.writeHead(statusCode, headers);
         res.end(data);
       } else {
         console.log('ERROR : ', err);
-      }      
+      }
     });
   }
   
