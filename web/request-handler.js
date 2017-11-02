cfb5d689954c55a228afd22f6052ed985b1a4ba7
Hack Reactor Student <student@hackreactor.com>
2017-11-01 19:59:05 -0700

    Plan writing to sites.txt

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,12 +4,17 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 
 exports.handleRequest = function (req, res) {
+  console.log('REQuest: ', req.method);
   if (req.url === '/') {
     var sitePath = archive.paths.siteAssets + '/index.html';
   } else {
+    // this is the archived site
     var sitePath = archive.paths.archivedSites + req.url;
+    // will need to modify how this is constructed if we
+    // use it for sending back site for the POST request.
   }
   
+  // if method === GET:
   if (sitePath) {
     fs.readFile(sitePath, (err, data) => {
       if (!err) {
@@ -21,5 +26,14 @@ exports.handleRequest = function (req, res) {
       }
     });
   }
+  
+  if (req.method === 'POST') {
+  // write to archive.paths.list
+  // use a request listener to grab data
+  // send back loading.html, and then the archived site
+    // fs.appendFile(archive.paths.list, req.url)
+    
+  }
+  
   // res.end(archive.paths.list); 
 };
